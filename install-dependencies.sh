a3f13c82acf52eb56915907ac46db285001a5b2e
Luis Felipe Mileo <mileo@kmee.com.br>
2017-10-19 09:31:05 -0200

    [FIX] Instalaço do libreoffice

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -52,9 +52,28 @@ apt-get update \
             postgresql-client* \
             default-jre \
             ure \
+            libreoffice \
             libreoffice-java-common \
             libreoffice-writer \
+            libreoffice-core \
+            libreoffice-common \
             fonts-symbola \
+            fonts-symbola \
+            fonts-opensymbol\
+            fonts-dejavu\
+            fonts-dejavu-core\
+            fonts-dejavu-extra\
+            fonts-noto\
+            fonts-dustin\
+            fonts-f500\
+            fonts-fanwood\
+            fonts-freefont-ttf\
+            fonts-liberation\
+            fonts-lmodern\
+            fonts-lyx\
+            fonts-sil-gentium\
+            fonts-texgyre\
+            fonts-tlwg-purisa\
         && locale-gen en_US en_US.UTF-8 pt_BR.UTF-8 \
         && echo "CURL iniciando..." \
         && curl -o wkhtmltox.deb -SL http://nightly.odoo.com/extra/wkhtmltox-0.12.1.2_linux-jessie-amd64.deb \
